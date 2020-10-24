2dbf3b672fdd8f813045413c2a8bcad84f605445
donald <donemurch@gmail.com>
2020-10-24 11:02:30 -0700

    Added 2 test cases to travic ci config to increase coverage from 68% to 85% - Basic tests for JSON Output

--- assignment1/run-tests.sh
+++ assignment1/run-tests.sh
@@ -8,3 +8,11 @@
 
 timeout 5 ./build/giftcardreader_coverage 1 ./hang.gft > /dev/null 
 [ $? -ne 0 ] && echo "Loop test failed" || echo "Loop test passed"
+
+./build/giftcardreader_coverage 2 examplefile.gft  > /dev/null 
+[ $? -ne 0 ] && echo "Example File JSON output failed" || echo "Example File JSON output passed"
+
+./build/giftcardreader_coverage 2 crash2.gft > /dev/null
+[ $? -ne 0 ] && echo "Heap Overwrite (JSON) test failed" || echo "Heap Overwrite test passed"
+
+
