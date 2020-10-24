051012f05aab49c8050a9f5f7a1572ffb73b775b
donald <donemurch@gmail.com>
2020-10-24 10:52:53 -0700

    Executed first coverage report using lcov

--- assignment1/run-tests.sh
+++ assignment1/run-tests.sh
@@ -1,10 +1,10 @@
 #/bin/bash
 
-./giftcardreader 1 crash1.gft > /dev/null 
+./build/giftcardreader_coverage 1 crash1.gft > /dev/null 
 [ $? -ne 0 ] && echo "Negative Read test failed" || echo "Negative Read test passed"
 
-./giftcardreader 1 crash2.gft > /dev/null
+./build/giftcardreader_coverage 1 crash2.gft > /dev/null
 [ $? -ne 0 ] && echo "Heap Overwrite test failed" || echo "Heap Overwrite test passed"
 
-timeout 5 ./giftcardreader 1 ./hang.gft > /dev/null 
+timeout 5 ./build/giftcardreader_coverage 1 ./hang.gft > /dev/null 
 [ $? -ne 0 ] && echo "Loop test failed" || echo "Loop test passed"
