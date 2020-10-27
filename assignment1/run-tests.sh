a66323b57941ae3bddc5755db3a4861698bc8e0e
donald <donemurch@gmail.com>
2020-10-26 20:17:16 -0700

    Updated run-tests.sh to test giftcardreader instead of giftcardreader_coverage

--- assignment1/run-tests.sh
+++ assignment1/run-tests.sh
@@ -1,30 +1,43 @@
 #/bin/bash
 
 failed=0
-./build/giftcardreader_coverage 1 ./tests/crash1.gft > /dev/null 
+./build/giftcardreader 1 ./tests/crash1.gft > /dev/null 
 if [[ $? -ne 0 ]]
 then
 	echo "Negative Read test failed"
         failed=1
 fi
 
-./build/giftcardreader_coverage 1 ./tests/crash2.gft > /dev/null
+./build/giftcardreader 1 ./tests/crash2.gft > /dev/null
 if [[ $? -ne 0 ]]
 then
 	echo "Heap Overwrite test failed"
 	failed=1
 fi
 
-timeout 5 ./build/giftcardreader_coverage 1 ./tests/hang.gft > /dev/null 
+timeout 5 ./build/giftcardreader 1 ./tests/hang.gft > /dev/null 
 if [[ $? -ne 0 ]]
 then
 	echo "Loop test failed"
 	failed=1
 fi
 
+./build/giftcardreader 1 ./tests/cov1.gft > /dev/null
+if [[ $? -ne 0 ]]
+then
+        echo "Simple print Amount Change record failed"
+        failed=1
+fi
+
+./build/giftcardreader 1 ./tests/cov2.gft > /dev/null
+if [[ $? -ne 0 ]]
+then
+        echo "Simple print Message record failed"
+        failed=1
+fi
 
 for test in ./afl_findings/queue/*; do
-	./build/giftcardreader_coverage 1 $test > /dev/null
+	./build/giftcardreader 1 $test > /dev/null
 	if [[ $? -ne 0 ]]
 	then
 		echo "QUEUE: "$(basename $test | cut -d"," -f1)" Failed"
@@ -34,7 +47,7 @@ done
 
 
 for test in ./afl_findings/crashes/*; do
-	./build/giftcardreader_coverage 1 $test > /dev/null
+	./build/giftcardreader 1 $test > /dev/null
 	if [[ $? -ne 0 ]]
 	then
 		echo "CRASH: "$(basename $test | cut -d"," -f1)" Failed"
