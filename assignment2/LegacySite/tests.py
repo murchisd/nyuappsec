7ca0942e59205da2569f3adf93a04e635c15297d
donald <donemurch@gmail.com>
2020-11-19 18:55:32 -0800

    Fixed XSS issues; details in bugs.txt; Added XSS tests to tests.py

--- assignment2/LegacySite/tests.py
+++ assignment2/LegacySite/tests.py
@@ -17,3 +17,19 @@ class CSRFTestClass(TestCase):
     #Make the POST request that are our weaponized link triggers 
     response = csrf_client.post('/gift/1', {'amount':'2000','username':'test2'})
     self.assertEqual(response.status_code,403)
+
+class XSSTestClass(TestCase):
+  def setUp(self):
+    p = Product(product_id = 1, product_name = "test", product_image_path = "/images/null", recommended_price = 200, description = "test product")
+    p.save()
+
+  def test_xss(self):
+    #Initialize client with CSRF checks
+    c = Client()
+    failed=0
+    #Make the GET request with XSS payload
+    r1 = c.get('/gift/1', {'director':'<script>alert(1)</script>'})
+    r2 = c.get('/buy',{'director':'<script>alert(1)</script>'}) 
+    #Check if the string was escaped
+    self.assertContains(r1,b"Endorsed by &lt;script&gt;alert(1)&lt;/script&gt;")
+    self.assertContains(r2,b"Endorsed by &lt;script&gt;alert(1)&lt;/script&gt;")
